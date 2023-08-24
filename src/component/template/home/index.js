2d0849429dea176898bccdfe64ea1d70414cf34a
Vinc370 <84572867+Vinc370@users.noreply.github.com>
2023-08-24 10:05:00 +0700

    Homepage

--- src/component/template/home/index.js
+++ src/component/template/home/index.js
@@ -23,6 +23,8 @@ import {
 
 import "./index.css"
 import "bootstrap/dist/css/bootstrap.min.css";
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'mdbreact/dist/css/mdb.css';
 
 let Home = (props) => {
     const ref = useRef(null);
@@ -41,19 +43,19 @@ let Home = (props) => {
                 className="bg-body-tertiary fixed-navbar">
       <Container>
         <Navbar.Brand href="#home">
-        <img src="https://www.amartek.id/i/logo/weblogo-amartek.png" height="40" />
+        <img src="https://www.amartek.id/i/logo/weblogo-amartek.png" height="40"/>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className='justify-content-end' style={{ width: "100%" }}>
             <Nav.Link href="https://www.amartek.id/">
-                <button type="button" class="btn btn-outline-primary">About Us</button>
+                <button type="button" class="btn btn-outline-primary btn-sm">About Us</button>
             </Nav.Link>
             <Nav.Link href="login">
-                <button type="button" class="btn btn-outline-success">Login</button>
+                <button type="button" class="btn btn-outline-success btn-sm">Login</button>
             </Nav.Link>
             <Nav.Link href="home">
-                <img src="https://cdn-icons-png.flaticon.com/512/74/74807.png" width="30" height="30" />
+                <img src="https://cdn-icons-png.flaticon.com/512/74/74807.png" width="32" height="32" style={{marginTop: "12%", marginLeft: "30%"}} />
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
@@ -62,16 +64,15 @@ let Home = (props) => {
     </div>
 
       <div name="mainpage" style={{backgroundImage: 'url("https://images.unsplash.com/photo-1623479322729-28b25c16b011?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fHByb2dyYW1tZXJ8ZW58MHx8MHx8fDA%3D&w=1000&q=80")', backgroundSize: "contain", backgroundSize: "100%", backgroundPositionY: "50%", textAlign: "center", color: "white", width: "100%", height: "50vh"}}>
-        <div style={{paddingTop: "160px"}}>
+        <div style={{paddingTop: "7%"}}>
         <h1 style={{textShadow: "0px 0px 30px white"}}>AMARTEK CODE TEST</h1>
         <p>A preliminary test for IT Graduate Development Program</p>
-        <Button variant="primary" className="mr-3" style={{marginRight: "0.25%"}}>Take an exam</Button>
-        <Button onClick={handleClick} variant="secondary" style={{marginLeft: "0.25%"}}>Learn More</Button>
+        <Button variant="primary" className="mr-3 btn-sm" style={{marginRight: "0.25%"}}>Take an exam</Button>
+        <Button onClick={handleClick} variant="secondary" className='btn-sm' style={{marginLeft: "0.25%"}}>Learn More</Button>
         </div>
-        <div>
-            <p></p>
+        <div style={{paddingTop: "5.8%"}}>
         </div>
-        <div ref={ref} style={{width: "70%", height: "100%", backgroundImage: 'url("https://media.discordapp.net/attachments/1107858899463327824/1143902893104185406/be0e8c88-2cc2-46d2-98e8-6390bc97e876.png?width=704&height=469")', backgroundSize: "contain", backgroundRepeat: "no-repeat"}}>
+        <div ref={ref} style={{width: "70%", height: "100%", backgroundImage: 'url("https://media.discordapp.net/attachments/1107858899463327824/1144102737705963560/tuxpi.com.1692845743.jpg?width=704&height=469")', backgroundSize: "contain", backgroundRepeat: "no-repeat"}}>
         <br />
         <h1 style={{color: "black", textAlign: "right"}}>
             Exam overview
@@ -92,7 +93,7 @@ let Home = (props) => {
         <div style={{width: "100%", height: "100%"}}>
         <p></p>
         <br />
-        <div style={{width: "35.6%", float: "right", height: "100%", backgroundImage: 'url("https://media.discordapp.net/attachments/1107858899463327824/1143911729705787412/tuxpi.com.1692800198.jpg?width=878&height=586")', backgroundSize: "contain", backgroundRepeat: "no-repeat"}}/>
+        <div style={{width: "35.6%", float: "right", height: "100%", backgroundImage: 'url("https://media.discordapp.net/attachments/1107858899463327824/1144102463599816845/tuxpi.com.1692845677.jpg?width=878&height=586")', backgroundSize: "contain", backgroundRepeat: "no-repeat"}}/>
         <div style={{width: "45%", float: "right"}}>
         <br /><br /><br /><br />
         <h2 style={{color: "black", textAlign: "justify"}}>
@@ -100,29 +101,64 @@ let Home = (props) => {
         </h2>
         </div>
         </div>
+        <div style={{width: '100%', height: '150%', marginTop: '5%', alignContent: "center", backgroundImage: 'url("https://media.discordapp.net/attachments/1107858899463327824/1144101544665555015/tuxpi.com.1692845458.jpg?width=878&height=586")', backgroundSize: '100%', backgroundRepeat: "no-repeat", backgroundPositionX: "center"}}>
+        <h1 style={{textShadow: "0px 0px 20px white", color: "white" , textAlign: "center", paddingTop: '14%'}}>
+            <b>
+            Take your test anytime
+            <br />
+            anywhere in a highly secure environment
+            </b>
+        </h1>
+        </div>
         <div style={{position: "relative", bottom: "0", width: "100%"}}>
         <MDBFooter bgColor='light' className='text-center text-lg-left'>
-      <MDBContainer className='p-4'>
-        <MDBRow>
-          <MDBCol lg='6' md='12' className='mb-4 mb-md-0'>
-            <h5 className='text-uppercase' style={{color: "black", textAlign: "left"}}>Footer text</h5>
+        <MDBContainer className='p-4'>
+          <MDBRow>
+            <MDBCol lg='6' md='12' className='mb-4 mb-md-0'>
+              <h5 className='text-uppercase' style={{color: "black", textAlign: "left"}}>Bumi Amartha Teknologi Mandiri</h5>
 
-            <p style={{color: "black", textAlign: "justify", fontSize: "90%"}}>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque ea quis molestias.
-              Fugiat pariatur maxime quis culpa corporis vitae repudiandae aliquam voluptatem veniam,
-              est atque cumque eum delectus sint!
-            </p>
+              <p style={{color: "black", textAlign: "justify", fontSize: "90%"}}>
+              PT. Bumi Amartha Teknologi Mandiri (AMARTEK) is a system integrator devoted to offering the highest value to our customers. Amartek provides domain and advisory expertise which comes with Data & Analytics, Outcome based Services, Integration & Automation, Talent Augmentation. Amartek was established in 2018 with the purpose of delivering a large stack of IT services globally.
+              </p>
+            </MDBCol>
+
+          <MDBCol lg='3' md='12' className='mb-4 mb-md-0'>
+            <h5 className='text-uppercase' style={{color: "black", textAlign: "left"}}>About Us</h5>
+            <ul className='list-unstyled mb-0' style={{color: "black", textAlign: "left"}}>
+                <li>
+                  <a style={{textDecoration: "None"}} href='https://www.amartek.id/about/overview' className='text-dark'>
+                    Company Overview
+                  </a>
+                </li>
+                <li>
+                  <a style={{textDecoration: "None"}} href='https://www.amartek.id/about/ceo-notes' className='text-dark'>
+                    CEO Notes
+                  </a>
+                </li>
+                <li>
+                  <a style={{textDecoration: "None"}} href='https://www.amartek.id/about/people' className='text-dark'>
+                    Executive Profiles
+                  </a>
+                </li>
+              </ul>
           </MDBCol>
 
-          <MDBCol lg='6' md='12' className='mb-4 mb-md-0'>
-            <h5 className='text-uppercase' style={{color: "black", textAlign: "left"}}>Footer text</h5>
-
-            <p style={{color: "black", textAlign: "justify", fontSize: "90%"}}>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque ea quis molestias.
-              Fugiat pariatur maxime quis culpa corporis vitae repudiandae aliquam voluptatem veniam,
-              est atque cumque eum delectus sint!
-            </p>
-          </MDBCol>
+          <MDBCol lg='3' md='12' className='mb-4 mb-md-0'>
+          <h5 className='text-uppercase' style={{color: "black", textAlign: "left"}}>Contact</h5>
+          <div>
+          <p style={{color: "black", textAlign: "left"}}>
+                <MDBIcon icon="home" className="me-2" />
+                Chase Plaza 9th Floor, Jl Jend. Sudirman Kav 21, Karet, Setiabudi, Jakarta Selatan 12920. Indonesia
+              </p>
+              <p style={{color: "black", textAlign: "left"}}>
+                <MDBIcon icon="envelope" className="me-3" />
+                  sales@batmandiri.com
+              </p>
+              <p style={{color: "black", textAlign: "left"}}>
+                <MDBIcon icon="phone" className="me-3" /> +62 21 3973 7000
+              </p>
+        </div>
+            </MDBCol>
         </MDBRow>
       </MDBContainer>
 
